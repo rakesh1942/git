e0eacf442ecc7a2faac0188dfd7cd4082280f6bd
rakesh1942 <rakeshkundarapu1942@gmail.com>
2025-02-14 13:07:57 +0000

    Updated rakesh.sh

--- rakesh.sh
+++ rakesh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-echo "This is my first project"
-
+echo "This is my first commit"
 echo "this is my feature branc0h"
 
 echo "This is my release branch"
+=======
+echo "this is my feature branch"
+
